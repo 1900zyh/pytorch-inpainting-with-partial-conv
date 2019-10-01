34beadc197d15c0ce0862d03541e5fca7da2dcaf
1900zyh <zengyh7@mail2.sysu.edu.cn>
2019-10-01 19:28:59 +0800

    debug:-

--- eval.py
+++ eval.py
@@ -20,7 +20,6 @@ parser = argparse.ArgumentParser(description='PyTorch Template')
 parser.add_argument('-r', '--resume', required=True)
 args = parser.parse_args()
 
-SPLIT = 5
 dims = 2048
 batch_size = 4
 block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[dims]
@@ -33,34 +32,32 @@ def main():
   fake_names.sort()
   # metrics prepare for image assesments
   metrics = {met: getattr(module_metric, met) for met in ['mae', 'psnr', 'ssim']}
-  step = len(real_names) // SPLIT
   # infer through videos
-  for idx in range(SPLIT):
-    real_images = []
-    fake_images = []
-    evaluation_scores = {key: 0 for key,val in metrics.items()}
-    for rname, fname in zip(real_names[idx*step: (idx+1)*step], fake_names[idx*step: (idx+1)*step]):
-      rimg = Image.open(rname)
-      fimg = Image.open(fname)
-      real_images.append(np.array(rimg))
-      fake_images.append(np.array(fimg))
-    # calculating image quality assessments
-    for key, val in metrics.items():
-      evaluation_scores[key] = val(real_images, fake_images)
-    print('[{}%-{}%]'.format((idx+1)*10, (idx+2)*10)+''.join(['{}: {:6f},'.format(key, val) for key,val in evaluation_scores.items()]))
-    
-    # calculate fid statistics for real images 
-    real_images = np.array(real_images).astype(np.float32)/255.0
-    real_images = real_images.transpose((0, 3, 1, 2))
-    real_m, real_s = calculate_activation_statistics(real_images, model, batch_size, dims)
-    
-    # calculate fid statistics for fake images
-    fake_images = np.array(fake_images).astype(np.float32)/255.0
-    fake_images = fake_images.transpose((0, 3, 1, 2))
-    fake_m, fake_s = calculate_activation_statistics(fake_images, model, batch_size, dims)
+  real_images = []
+  fake_images = []
+  evaluation_scores = {key: 0 for key,val in metrics.items()}
+  for rname, fname in zip(real_names, fake_names):
+    rimg = Image.open(rname)
+    fimg = Image.open(fname)
+    real_images.append(np.array(rimg))
+    fake_images.append(np.array(fimg))
+  # calculating image quality assessments
+  for key, val in metrics.items():
+    evaluation_scores[key] = val(real_images, fake_images)
+  print(' '.join(['{}: {:6f},'.format(key, val) for key,val in evaluation_scores.items()]))
+  
+  # calculate fid statistics for real images 
+  real_images = np.array(real_images).astype(np.float32)/255.0
+  real_images = real_images.transpose((0, 3, 1, 2))
+  real_m, real_s = calculate_activation_statistics(real_images, model, batch_size, dims)
+  
+  # calculate fid statistics for fake images
+  fake_images = np.array(fake_images).astype(np.float32)/255.0
+  fake_images = fake_images.transpose((0, 3, 1, 2))
+  fake_m, fake_s = calculate_activation_statistics(fake_images, model, batch_size, dims)
 
-    fid_value = calculate_frechet_distance(real_m, real_s, fake_m, fake_s)
-    print('[{}%-{}%]  FID: {}'.format((idx+1)*10, (idx+2)*10, round(fid_value, 5)))
+  fid_value = calculate_frechet_distance(real_m, real_s, fake_m, fake_s)
+  print('FID: {}'.format(round(fid_value, 5)))
   print('Finish evaluation from {}'.format(args.resume))
   
 
